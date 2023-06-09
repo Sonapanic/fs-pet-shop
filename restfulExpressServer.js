ed0f288021777509a9f5c81001e8fd1bde6e4d3f
Blair Jones <monogator@gmail.com>
2023-06-09 09:16:04 -0600

    MVP for assignment 1

--- restfulExpressServer.js
+++ restfulExpressServer.js
@@ -7,17 +7,15 @@ petShopApp.use(express.urlencoded({ extended: false}))
 const petsPath = path.join(__dirname, 'pets.json')
 petShopApp.use(express.json())
 
-
-petShopApp.get('/pets/', (req, res) => {
-    fs.readFile(petsPath, (err, data) => {
-        err ? console.error(err) : res.status(200).send(JSON.parse(data))
-    })
-})
-// CONSOLIDATE WHEN ABLE
-petShopApp.get('/pets', (req, res) => {
-    fs.readFile(petsPath, (err, data) => {
-        err ? console.error(err) : res.status(200).send(JSON.parse(data))
-    })
+petShopApp.get('/:id', (req, res) => {
+    let id = req.params.id
+    if (id === 'pets/' || id === 'pets') {
+        fs.readFile(petsPath, (err, data) => {
+            err ? console.error(err) : res.status(200).send(JSON.parse(data))
+        })
+    } else {
+        res.status(404).send('Not found')
+    }
 })
 
 petShopApp.get('/pets/:id', (req, res) => {
@@ -33,7 +31,6 @@ petShopApp.get('/pets/:id', (req, res) => {
         }
     })
 })
-
 
 petShopApp.post('/pets', (req, res) => {
     fs.readFile(petsPath, (err, data) => {
@@ -57,46 +54,30 @@ petShopApp.post('/pets', (req, res) => {
     })
 })
 
-
 petShopApp.patch('/pets/:id', (req, res) => {
     fs.readFile(petsPath, (err, data) => {
-        const petData = JSON.parse(data)
+        let petData = JSON.parse(data)
         if (err) {
-            throw err
+            console.error(err)
         } else {
-            const newPet = req.body
-            const id = req.params.id
-            if (newPet.age && newPet.kind && newPet.name) {
-                petData[id].age = newPet.age
-                petData[id].kind = newPet.kind
-                petData[id].name = newPet.name
-            } else if (newPet.kind && newPet.age) {
-                petData[id].kind = newPet.kind
-                petData[id].age = newPet.age
-            } else if (newPet.name && newPet.kind) {
-                petData[id].name = newPet.name
-                petData[id].kind = newPet.kind
-            } else if (newPet.age && newPet.name) {
-                petData[id].age = newPet.age
-                petData[id].name = newPet.name
-            } else if (newPet.age) { 
-                petData[id].age = newPet.age
-            } else if (newPet.kind) {
-                petData[id].kind = newPet.kind
-            } else if (newPet.name) {
-                petData[id].name = newPet.name
-            } else if (newPet = {}) {
-                res.status(400).send('Forgot data, you silly goose')
+            let newPet = req.body
+            let id = req.params.id
+            if (newPet.age || newPet.kind || newPet.name) {
+                for (let key in petData[id]) {
+                    if (newPet[key]) {
+                        petData[id][key] = newPet[key]
+                    }
+                    
+                }
+                fs.writeFile(petsPath, JSON.stringify(petData), (err) => {
+                    err ? console.error(err) : res.status(200).send(petData)
+                })
             } else {
-                res.status(400).send('IDEK what you\'re trying to do here')
+                res.status(400).send('Bad request')
             }
-            fs.writeFile(petsPath, JSON.stringify(petData), (err) => {
-                err ? console.error(err) : res.send(petData)
-            })
         } 
     }) 
 })
-
 
 petShopApp.delete('/pets/:id', (req, res) => {
     fs.readFile(petsPath, (err, data) => {
@@ -118,6 +99,9 @@ petShopApp.delete('/pets/:id', (req, res) => {
     })
 })
 
+petShopApp.use((req, res) => {
+    res.status(404).send('Not found')
+})
 
 petShopApp.listen(port, () => {
     console.log('Server\'s up on', port)
